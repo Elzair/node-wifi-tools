98dc360f54567fcfc4a9f7f3884bc4fd7a213531
Elzair <elzairthesorcerer@gmail.com>
2014-06-10 14:54:46 -0400

    Added parser for netstat under OSX

--- index.js
+++ index.js
@@ -1,41 +1,37 @@
 var ap_parse = require(__dirname + '/src/airport_parser')
-  , cb2y     = require('cb2yield')
   , exec     = require('child_process').exec
   , os       = require('os')
   , nm_parse = require(__dirname + '/src/nm_parser')
+  , ns_parse = require(__dirname + '/src/ns_parser')
   , util     = require('util')
   ;
 
-//var results = cb2y(exec, ['nm-tool', {cb2yield_cb_placeholder: true}]);
-function *get_info() {
-  if (os.platform() === 'linux') {
-    var devices = yield cb2y(exec, ['nm-tool']);
-    console.log(util.format('%j', devices));
-    //exec('nm-tool', function(error, stdout, stderr) {
-    //  console.log('stderr: ' + stderr);
-    //  if (error !== null) {
-    //    console.log('exec error: ' + error);
-    //  }
-    //  var devices = nm_parse(stdout);
-    //  console.log(util.format('%j', devices));
-    //});
-  }
-  else if (os.platform() === 'darwin') {
-    var out = yield cb2y(exec, ['/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport --getinfo']);
-    console.log(util.format('%j', out));
-    //exec('/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport --getinfo', 
-    //    function(error, stdout, stderr) {
-    //  console.log('stderr: ' + stderr);
-    //  if (error !== null) {
-    //    console.log('exec error: ' + error);
-    //  }
-    //  var devices = ap_parse(stdout);
-    //  console.log(util.format('%j', devices));
-    //});
-  }
-  else {
-    console.error(util.format('Unsupported platform: %s!', os.platform()));
-  }
+//var results = cb2y(exec, ['nm-tool']);
+if (os.platform() === 'linux') {
+  exec('nm-tool', function(error, stdout, stderr) {
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+    var nm_out = nm_parse(stdout);
+    console.log(JSON.stringify(nm_out, null, 2));
+  });
 }
-
-get_info();
+else if (os.platform() === 'darwin') {
+  exec('/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport --getinfo', 
+      function(error, stdout, stderr) {
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+    var ap_out = ap_parse(stdout);
+    exec('netstat -rn', function(nerror, nstdout, nstderr) {
+      var ns_out = ns_parse(nstdout);
+      console.log(JSON.stringify(ap_out, null, 2));
+      console.log(JSON.stringify(ns_out, null, 2));
+    });
+  });
+}
+else {
+  console.error(util.format('Unsupported platform: %s!', os.platform()));
+}
---
+++ src/ns_parser.js
@@ -0,0 +1,51 @@
+var s = require('string');
+
+module.exports = function(input) {
+  var inp = s(input).lines();
+  var routes = {internet: [], internet6: []};
+  var in_i = false, in_i6 = false, skip_next = false;
+
+  for (var i =0; i<inp.length; i++) {
+    var line = inp[i];
+    if (s(line).startsWith('Internet:')) {
+      in_i = true;
+      in_i6 = false;
+      skip_next = true;
+    }
+    else if (s(line).startsWith('Internet6')) {
+      in_i = false;
+      in_i6 = true;
+      skip_next = true;
+    }
+    else if (line === '') {
+      continue;
+    }
+    else if (skip_next === true) {
+      skip_next = false;
+    }
+    else if (in_i) {
+      var ii = line.split(/\s+/g);
+      routes.internet.push({
+          destination: ii[0].trim()
+        , gateway: ii[1].trim()
+        , flags: ii[2].trim()
+        , refs: ii[3].trim()
+        , use: ii[4].trim()
+        , netif: ii[5].trim()
+        , expire: ii[6] ? ii[6].trim() : null
+      });
+    }
+    else if (in_i6) {
+      var i6 = line.split(/\s+/g);
+      routes.internet6.push({
+          destination: i6[0]
+        , gateway: i6[1].trim()
+        , flags: i6[2].trim()
+        , netif: i6[3].trim()
+        , expire: i6[4] ? i6[4].trim() : null
+      });
+    }
+  }
+
+  return routes;
+};
