8342063d25c2282ae9df390da296a429a548e05e
Elzair <elzairthesorcerer@gmail.com>
2014-06-11 15:03:00 -0400

    Split project into a module (index.js) and an executable shellscript (bin/node-wifi-tools)

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-Node Wifi Info
-==============
+# Node Wifi Tools
 
 Utility for [Node.js](http://nodejs.org) to work with 802.11 interfaces 
+
+## Installation
+
+You can use **node-wifi-tools** as either a standalone "executable" or as a [npm](https://www.npmjs.org/) module.
+
+### Module
+
+Install **node-wifi-tools** into your project.
+
+    npm install node-wifi-tools --save
+
+### Standalone
+
+Install **node-wifi-tools** globally on your system
+
+    npm install node-wifi-tools -g
+
+The command **node-wifi-tools** has been added to your global *node_modules/* directory and a link to *bin/node-wifi-tools* has been added to your **$PATH**. 
--- index.js
+++ index.js
@@ -3,35 +3,43 @@ var ap_parse = require(__dirname + '/src/airport_parser')
   , os       = require('os')
   , nm_parse = require(__dirname + '/src/nm_parser')
   , ns_parse = require(__dirname + '/src/ns_parser')
-  , util     = require('util')
   ;
 
-//var results = cb2y(exec, ['nm-tool']);
-if (os.platform() === 'linux') {
-  exec('nm-tool', function(error, stdout, stderr) {
-    console.log('stderr: ' + stderr);
-    if (error !== null) {
-      console.log('exec error: ' + error);
-    }
-    var nm_out = nm_parse(stdout);
-    console.log(JSON.stringify(nm_out, null, 2));
-  });
+function isFunction(func) {
+  return func && {}.toString.call(func) === '[object Function]';
 }
-else if (os.platform() === 'darwin') {
-  exec('/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport --getinfo', 
-      function(error, stdout, stderr) {
-    console.log('stderr: ' + stderr);
-    if (error !== null) {
-      console.log('exec error: ' + error);
-    }
-    var ap_out = ap_parse(stdout);
-    exec('netstat -rn', function(nerror, nstdout, nstderr) {
-      var ns_out = ns_parse(nstdout);
-      console.log(JSON.stringify(ap_out, null, 2));
-      console.log(JSON.stringify(ns_out, null, 2));
+
+exports.get_info = function(cb) {
+  // First ensure callback is a valid function
+  if (!isFunction(cb)) {
+    throw "Invalid function passed to get_info()!";
+  }
+
+  if (os.platform() === 'linux') {
+    exec('nm-tool', function(error, stdout, stderr) {
+      if (error || stderr || stderr !== '') {
+        cb({error: error, stderr: stderr}, null);
+      }
+      cb(null, nm_parse(stdout));
     });
-  });
-}
-else {
-  console.error(util.format('Unsupported platform: %s!', os.platform()));
-}
+  }
+  else if (os.platform() === 'darwin') {
+    exec('/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport --getinfo', 
+        function(error, stdout, stderr) {
+      if (error || stderr || stderr !== '') {
+        cb({error: error, stderr: stderr}, null);
+      }
+      var ap_out = ap_parse(stdout);
+      exec('netstat -rn', function(nerror, nstdout, nstderr) {
+        if (nerror || nstderr || nstderr !== '') {
+          cb({error: nerror, stderr: nstderr}, null);
+        }
+        var ns_out = ns_parse(nstdout);
+        cb(null, {access_point: ap_out, netstat: ns_out});
+      });
+    });
+  }
+  else {
+    cb("Unsupported platform: " + os.platform(), null);
+  }
+};
