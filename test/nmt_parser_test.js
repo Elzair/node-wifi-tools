833ce91796b1c6f9df93254fdeac84bb8be79a95
Philip Woods <elzairthesorcerer@gmail.com>
2014-06-18 10:07:21 -0400

    Fixed bugs in nm-tool parser and netsh parser and finished unit tests for nm-tool parser

--- test/nmt_parser_test.js
+++ test/nmt_parser_test.js
@@ -34,7 +34,7 @@ describe('nmt_parser', function() {
       '    myTestAP:      Infra, 66:7C:CF:7E:48:EF, Freq 2462 MHz, Rate 54 Mb/s, Strength 59 WPA WPA2',
       '    *test01:    Infra, 97:70:69:86:A7:9F, Freq 5180 MHz, Rate 54 Mb/s, Strength 69',
       '    DevTeam: Infra, 2D:A6:02:B0:BF:82, Freq 2412 MHz, Rate 54 Mb/s, Strength 100 WPA2',
-      '    test01:     Infra, F9:BD:D1:5E:F8:66, Freq 2437 MHz, Rate 54 Mb/s, Strength 100',
+      '    test02:     Infra, F9:BD:D1:5E:F8:66, Freq 2437 MHz, Rate 54 Mb/s, Strength 100',
       '    TestNet:          Infra, 42:62:87:4E:F8:2C, Freq 2462 MHz, Rate 54 Mb/s, Strength 87 WPA WPA2',
       '',
       '  IPv4 Settings:',
@@ -103,6 +103,54 @@ describe('nmt_parser', function() {
 
     it('should display the correct connected access point (without the asterisk)', function *() {
       assert.equal(info[0].connected_ap, 'test01');
+    });
+
+    it('should display the SSID of the connected network', function *() {
+      assert.equal(info[0].wireless_access_points.test01.ssid, 'test01');
+    });
+
+    it('should display the network type of the connected network', function *() {
+      assert.equal(info[0].wireless_access_points.test01.network_type, 'Infra');
+    });
+
+    it('should display the BSSID of the connected network', function *() {
+      assert.equal(info[0].wireless_access_points.test01.bssid, '97:70:69:86:A7:9F');
+    });
+
+    it('should display the frequency of the connected network', function *() {
+      assert.equal(info[0].wireless_access_points.test01.frequency, '5180 MHz');
+    });
+
+    it('should display the rate of the connected network', function *() {
+      assert.equal(info[0].wireless_access_points.test01.rate, '54 Mb/s');
+    });
+
+    it('should display the strength of the connected network', function *() {
+      assert.equal(info[0].wireless_access_points.test01.strength, '69');
+    });
+
+    it('should display the encryption modes of the connected network', function *() {
+      assert.equal(info[0].wireless_access_points.test01.encryption, false);
+    });
+
+    it('should display the IPv4 address of the interface', function *() {
+      assert.equal(info[0].ipv4_settings.address, '192.168.165.84');
+    });
+
+    it('should display the IPv4 prefix of the interface', function *() {
+      assert.equal(info[0].ipv4_settings.prefix, '24 (255.255.255.0)');
+    });
+
+    it('should display the IPv4 gateway of the interface', function *() {
+      assert.equal(info[0].ipv4_settings.gateway, '192.168.191.1');
+    });
+
+    it('should display the first DNS server for the interface', function *() {
+      assert.equal(info[0].ipv4_settings.dns, '207.68.222.222');
+    });
+
+    it('should display the second DNS server for the interface', function *() {
+      assert.equal(info[0].ipv4_settings.dns_1, '207.68.220.220');
     });
   });
 });
