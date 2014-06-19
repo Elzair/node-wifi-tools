75ceb8c2172086fed7e1e699deb625d8e2a83f21
Philip Woods <elzairthesorcerer@gmail.com>
2014-06-19 10:23:49 -0400

    Added os specific functions for get_info

--- index.js
+++ index.js
@@ -8,27 +8,73 @@ var ap_parse     = require(__dirname + '/lib/airport_parser')
   , spawn        = require('co-child-process')
   ;
 
-exports.get_info = function*(cb) {
-  if (os.platform() === 'linux') {
-    var nt_out = yield spawn('nm-tool');
-    return {network_tool: nmt_parse(nt_out)};
+var get_info_linux = exports.get_info_linux = function *() {
+  if (os.platform() !== 'linux') {
+    throw "Platform is not linux";
   }
-  else if (os.platform() === 'darwin') {
-    var ap_out = yield spawn('/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport', ['--getinfo']);
-    var nst_out = yield spawn('netstat', ['-rn']);
-    return {airport: ap_parse(ap_out), netstat: nst_parse(nst_out)};
+
+  var nt_out = yield spawn('nm-tool');
+  return {network_tool: nmt_parse(nt_out)};
+};
+
+var get_info_darwin = exports.get_info_darwin = function *() {
+  if (os.platform() !== 'darwin') {
+    throw "Platform is not darwin";
   }
-  else if (os.platform() === 'windows') {
-    var nsh_ip_out = yield spawn('netsh', ['interface', 'ip', 'show', 'address']);
-    var nsh_wi_out = yield spawn('netsh', ['wlan', 'show', 'interfaces']);
-    var nsh_wn_out = yield spawn('netsh', ['wlan', 'show', 'networks', 'mode=bssid']);
-    return {
-        ip_show_address: nsh_ip_parse(nsh_ip_out)
-      , wlan_show_interfaces: nsh_wi_parse(nsh_wi_out)
-      , wlans_show_networks: nsh_wn_parse(nsh_wn_out)
-    };
+
+  var ap_out = yield spawn('/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport', ['--getinfo']);
+  var nst_out = yield spawn('netstat', ['-rn']);
+  return {airport: ap_parse(ap_out), netstat: nst_parse(nst_out)};
+};
+
+var get_info_windows = exports.get_info_windows = function *() {
+  if (os.platform() !== 'windows') {
+    throw "Platform is not windows";
   }
-  else {
-    throw "Unsupported platform: " + os.platform();
+
+  var nsh_ip_out = yield spawn('netsh', ['interface', 'ip', 'show', 'address']);
+  var nsh_wi_out = yield spawn('netsh', ['wlan', 'show', 'interfaces']);
+  var nsh_wn_out = yield spawn('netsh', ['wlan', 'show', 'networks', 'mode=bssid']);
+  return {
+      ip_show_address: nsh_ip_parse(nsh_ip_out)
+    , wlan_show_interfaces: nsh_wi_parse(nsh_wi_out)
+    , wlans_show_networks: nsh_wn_parse(nsh_wn_out)
+  };
+};
+
+exports.get_info = function*(op_sys) {
+  var out = null;
+
+  switch(op_sys) {
+    case 'linux':
+      out = yield get_info_linux();
+      break;
+    case 'darwin':
+    case 'osx':
+      out = yield get_info_darwin();
+      break;
+    case 'windows':
+      out = yield get_info_windows();
+      break;
   }
+
+  if (out) {
+    return out;
+  }
+
+  switch(os.platform()) {
+    case 'linux':
+      out = yield get_info_linux();
+      break;
+    case 'darwin':
+      out = yield get_info_darwin();
+      break;
+    case 'windows':
+      out = yield get_info_windows();
+      break;
+    default:
+      throw "Unsupported platform: " + os.platform();
+  }
+
+  return out;
 };
--- test/main_test.js
+++ test/main_test.js
@@ -5,6 +5,66 @@ var assert   = require('assert')
   ;
 
 describe('node-wifi-tools', function() {
+  describe('get_info_linux()', function() {
+    var info;
+
+    it('should have an info object if the system is linux and should throw an error otherwise', function *() {
+      if (os.platform() === 'linux') {
+        info = yield nw_tools.get_info_linux();
+        assert.strictEqual(info.hasOwnProperty('network_tool'), true);
+      }
+      else {
+        try {
+          info = yield nw_tools.get_info_linux();
+        }
+        catch (e) {
+          assert.strictEqual(e, 'Platform is not linux');
+        }
+      }
+    });
+  });
+
+  describe('get_info_darwin()', function() {
+    var info;
+
+    it('should have an info object if the system is darwin and should throw an error otherwise', function *() {
+      if (os.platform() === 'darwin') {
+        info = yield nw_tools.get_info_darwin();
+        assert.strictEqual(info.hasOwnProperty('airport'), true);
+        assert.strictEqual(info.hasOwnProperty('netstat'), true);
+      }
+      else {
+        try {
+          info = yield nw_tools.get_info_darwin();
+        }
+        catch (e) {
+          assert.strictEqual(e, 'Platform is not darwin');
+        }
+      }
+    });
+  });
+
+  describe('get_info_windows()', function() {
+    var info;
+
+    it('should have an info object if the system is windows and should throw an error otherwise', function *() {
+      if (os.platform() === 'windows') {
+        info = yield nw_tools.get_info_windows();
+        assert.strictEqual(info.hasOwnProperty('ip_show_address'), true);
+        assert.strictEqual(info.hasOwnProperty('wlan_show_interfaces'), true);
+        assert.strictEqual(info.hasOwnProperty('wlans_show_networks'), true);
+      }
+      else {
+        try {
+          info = yield nw_tools.get_info_windows();
+        }
+        catch (e) {
+          assert.strictEqual(e, 'Platform is not windows');
+        }
+      }
+    });
+  });
+
   describe('get_info()', function() {
     var info;
 
@@ -12,16 +72,67 @@ describe('node-wifi-tools', function() {
       info = yield nw_tools.get_info();
 
       assert.strictEqual(typeof info, 'object');
+      switch(os.platform()) {
+        case 'linux':
+          assert.strictEqual(info.hasOwnProperty('network_tool'), true);
+          break;
+        case 'darwin':
+          assert.strictEqual(info.hasOwnProperty('airport'), true);
+          assert.strictEqual(info.hasOwnProperty('netstat'), true);
+          break;
+        case 'windows':
+          assert.strictEqual(info.hasOwnProperty('ip_show_address'), true);
+          assert.strictEqual(info.hasOwnProperty('wlan_show_interfaces'), true);
+          assert.strictEqual(info.hasOwnProperty('wlans_show_networks'), true);
+          break;
+        default:
+          assert.strictEqual(info, null);
+      }
+    });
+
+    it('should have an info object with linux properties or throw an error', function*() {
       if (os.platform() === 'linux') {
+        info = yield nw_tools.get_info('linux');
         assert.strictEqual(info.hasOwnProperty('network_tool'), true);
       }
-      else if (os.platform() === 'darwin') {
-        assert.strictEqual(info.hasOwnProperty('airport'), true);
-        assert.strictEqual(info.hasOwnProperty('netstat'), true);
+      else {
+        try {
+          info = yield nw_tools.get_info('linux');
+        }
+        catch (e) {
+          assert.strictEqual(e, 'Platform is not linux');
+        }
+      }
+    });
+
+    it('should have an info object with darwin properties or throw an error', function*() {
+      if (os.platform() === 'darwin') {
+        info = yield nw_tools.get_info('darwin');
+        assert.strictEqual(info.hasOwnProperty('network_tool'), true);
       }
       else {
-        assert.strictEqual(info, null);
+        try {
+          info = yield nw_tools.get_info('darwin');
+        }
+        catch (e) {
+          assert.strictEqual(e, 'Platform is not darwin');
+        }
       }
-    })
+    });
+
+    it('should have an info object with windows properties or throw an error', function*() {
+      if (os.platform() === 'windows') {
+        info = yield nw_tools.get_info('windows');
+        assert.strictEqual(info.hasOwnProperty('network_tool'), true);
+      }
+      else {
+        try {
+          info = yield nw_tools.get_info('windows');
+        }
+        catch (e) {
+          assert.strictEqual(e, 'Platform is not windows');
+        }
+      }
+    });
   });
 });
