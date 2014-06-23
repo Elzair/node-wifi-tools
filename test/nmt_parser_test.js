451dc725c048677af5948506c4183e6c98fffbd3
Philip Woods <elzairthesorcerer@gmail.com>
2014-06-23 12:29:09 -0400

    Modified networkmanager parser to return devices as object instead of array

--- test/nmt_parser_test.js
+++ test/nmt_parser_test.js
@@ -65,92 +65,96 @@ describe('nmt_parser', function() {
   });
 
   describe('Wireless Device', function () {
+    it('should contain the correct device', function *() {
+      assert.ok(info.hasOwnProperty('wlan0'));
+    });
+
     it('should display correct device name', function *() {
-      assert.strictEqual(info[0].name, 'wlan0');
+      assert.strictEqual(info.wlan0.name, 'wlan0');
     });
 
     it('should display correct device type', function *() {
-      assert.strictEqual(info[0].type, '802.11 WiFi');
+      assert.strictEqual(info.wlan0.type, '802.11 WiFi');
     });
 
     it('should display correct driver name', function *() {
-      assert.strictEqual(info[0].driver, 'iwlwifi');
+      assert.strictEqual(info.wlan0.driver, 'iwlwifi');
     });
 
     it('should display that it is connected', function *() {
-      assert.strictEqual(info[0].state, 'connected');
+      assert.strictEqual(info.wlan0.state, 'connected');
     });
 
     it('should display that it the default', function *() {
-      assert.strictEqual(info[0].default, 'yes');
+      assert.strictEqual(info.wlan0.default, 'yes');
     });
 
     it('should display the correct MAC address', function *() {
-      assert.strictEqual(info[0].hw_address, '91:5B:CE:AF:F9:E6');
+      assert.strictEqual(info.wlan0.hw_address, '91:5B:CE:AF:F9:E6');
     });
 
     it('should display the device supports WEP encryption', function *() {
-      assert.strictEqual(info[0].wireless_properties.wep_encryption, 'yes');
+      assert.strictEqual(info.wlan0.wireless_properties.wep_encryption, 'yes');
     });
 
     it('should display the device supports WPA encryption', function *() {
-      assert.strictEqual(info[0].wireless_properties.wpa_encryption, 'yes');
+      assert.strictEqual(info.wlan0.wireless_properties.wpa_encryption, 'yes');
     });
 
     it('should display the device supports WPA2 encryption', function *() {
-      assert.strictEqual(info[0].wireless_properties.wpa2_encryption, 'yes');
+      assert.strictEqual(info.wlan0.wireless_properties.wpa2_encryption, 'yes');
     });
 
     it('should display the correct connected access point (without the asterisk)', function *() {
-      assert.strictEqual(info[0].connected_ap, 'test01');
+      assert.strictEqual(info.wlan0.connected_ap, 'test01');
     });
 
     it('should display the SSID of the connected network', function *() {
-      assert.strictEqual(info[0].wireless_access_points.test01.ssid, 'test01');
+      assert.strictEqual(info.wlan0.wireless_access_points.test01.ssid, 'test01');
     });
 
     it('should display the network type of the connected network', function *() {
-      assert.strictEqual(info[0].wireless_access_points.test01.network_type, 'Infra');
+      assert.strictEqual(info.wlan0.wireless_access_points.test01.network_type, 'Infra');
     });
 
     it('should display the BSSID of the connected network', function *() {
-      assert.strictEqual(info[0].wireless_access_points.test01.bssid, '97:70:69:86:A7:9F');
+      assert.strictEqual(info.wlan0.wireless_access_points.test01.bssid, '97:70:69:86:A7:9F');
     });
 
     it('should display the frequency of the connected network', function *() {
-      assert.strictEqual(info[0].wireless_access_points.test01.frequency, '5180000000');
+      assert.strictEqual(info.wlan0.wireless_access_points.test01.frequency, '5180000000');
     });
 
     it('should display the rate of the connected network', function *() {
-      assert.strictEqual(info[0].wireless_access_points.test01.rate, '54 Mb/s');
+      assert.strictEqual(info.wlan0.wireless_access_points.test01.rate, '54 Mb/s');
     });
 
     it('should display the strength of the connected network', function *() {
-      assert.strictEqual(info[0].wireless_access_points.test01.strength, '69');
+      assert.strictEqual(info.wlan0.wireless_access_points.test01.strength, '69');
     });
 
     it('should display the number of encryption modes supported by the connected network', function *() {
-      assert.strictEqual(info[0].wireless_access_points.test01.encryption.length, 0);
+      assert.strictEqual(info.wlan0.wireless_access_points.test01.encryption.length, 0);
     });
 
     it('should display the IPv4 address of the interface', function *() {
-      assert.strictEqual(info[0].ipv4_settings.address, '192.168.165.84');
+      assert.strictEqual(info.wlan0.ipv4_settings.address, '192.168.165.84');
     });
 
     it('should display the IPv4 prefix of the interface', function *() {
-      assert.strictEqual(info[0].ipv4_settings.prefix, '24 (255.255.255.0)');
+      assert.strictEqual(info.wlan0.ipv4_settings.prefix, '24 (255.255.255.0)');
     });
 
     it('should display the IPv4 gateway of the interface', function *() {
-      assert.strictEqual(info[0].ipv4_settings.gateway, '192.168.191.1');
+      assert.strictEqual(info.wlan0.ipv4_settings.gateway, '192.168.191.1');
     });
 
     it('should display the first DNS server for the interface', function *() {
-      assert.strictEqual(info[0].ipv4_settings.dns, '207.68.222.222');
+      assert.strictEqual(info.wlan0.ipv4_settings.dns, '207.68.222.222');
     });
 
     it('should display the second DNS server for the interface', function *() {
-      assert.strictEqual(info[0].ipv4_settings.dns_1, '207.68.220.220');
+      assert.strictEqual(info.wlan0.ipv4_settings.dns_1, '207.68.220.220');
     });
   });
 });
